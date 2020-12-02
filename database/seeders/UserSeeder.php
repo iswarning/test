e3b6a506d2dfa079369d51f241af7ac139704af6
iswarning <59012163+iswarning@users.noreply.github.com>
2020-12-02 16:07:02 +0700

    new

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Modes\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+           'name' => 'admin',
+           'email' => 'admin@gmail.com',
+           'password' => bcrypt('123456') ,
+           'role' => 1
+        ]);
+    }
+}
